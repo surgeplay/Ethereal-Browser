4a723b27b9b73d54767362dfb8b97240e15abc03
Aesen Vismea <aesen.vismea@gmail.com>
2014-12-09 18:46:18 -0500

    Basic UI

--- src/main/java/com/gameminers/ethereal/browser/EtherealBrowser.java
+++ src/main/java/com/gameminers/ethereal/browser/EtherealBrowser.java
@@ -17,13 +17,137 @@
  */
 package com.gameminers.ethereal.browser;
 
+import java.io.File;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JTabbedPane;
+import javax.swing.UIManager;
+import javax.swing.WindowConstants;
+
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import joptsimple.OptionSpec;
 
 public class EtherealBrowser {
-
+	private static File minecraftDirectory;
 	public static void main(String[] args) {
-		JFrame window = new JFrame("EtherealBrowser");
+		OptionParser parser = new OptionParser();
+		OptionSpec<File> dir = parser.accepts("d", "The Minecraft directory; defaults to the platform-specific .minecraft").withRequiredArg().ofType(File.class);
+		
+		OptionSet set = parser.parse(args);
+		if (set.has(dir)) {
+			minecraftDirectory = set.valueOf(dir);
+		} else {
+			minecraftDirectory = Paths.getAppData("minecraft");
+		}
+		initLAF();
+		JFrame window = createWindow();
+		
+		JTabbedPane tabs = new JTabbedPane();
+		window.add(tabs);
+		
+		window.setJMenuBar(createMenuBar());
+		
+		tabs.addTab("Welcome", createWelcomeScreen());
+		
+		window.setVisible(true);
+	}
 
+	private static void initLAF() {
+		try {
+			// try to set GTK+ first so we don't look hideous on Linux
+			// XXX - on a Windows system, will this set GTK+ if it's installed?
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
+		} catch (Exception e) {
+			try {
+				// otherwise, do what this JVM thinks is best
+				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			} catch (Exception ex) {}
+		}
+	}
+
+	private static JFrame createWindow() {
+		JFrame window = new JFrame("Ethereal Browser");
+		window.setIconImages(Resources.loadPNGAsset("browser", "browser-32"));
+		window.setSize(854, 480);
+		window.setLocationByPlatform(true);
+		window.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+		window.addWindowListener(new MainWindowListener());
+		return window;
+	}
+	
+	private static Box createWelcomeScreen() {
+		Box box = Components.createPaddedBox(BoxLayout.Y_AXIS);
+		box.add(new JLabel("Welcome to Ethereal Browser!"));
+		box.add(new JLabel("To get started, open the File menu and choose either Open Modded, or Open Vanilla."));
+		box.add(new JLabel("If neither is enabled, no assets can be found in your Minecraft directory. This could mean it is invalid, or you've never launched Minecraft."));
+		box.add(Box.createVerticalStrut(24));
+		box.add(new JLabel("The current Minecraft directory is "+Paths.tildize(getMinecraftDirectory().getAbsolutePath())));
+		return box;
+	}
+
+	public static File getMinecraftDirectory() {
+		return minecraftDirectory;
+	}
+
+	private static JMenuBar createMenuBar() {
+		JMenuBar bar = new JMenuBar();
+		bar.add(createFileMenu());
+		return bar;
+	}
+
+	private static JMenu createFileMenu() {
+		JMenu file = new JMenu("File");
+		file.add(createOpenVanillaMenu());
+		file.add(createOpenModdedMenu());
+		return file;
+	}
+
+	private static JMenu createOpenModdedMenu() {
+		JMenu menu = new JMenu("Open Modded");
+		menu.setIcon(Resources.loadPNGIconAsset("open-modded"));
+		File mods = new File(minecraftDirectory, "mods");
+		if (mods.exists()) {
+			boolean global = false;
+			for (File f : Paths.sort(mods.listFiles())) {
+				String name = f.getName();
+				if (f.isDirectory() && (name.contains(".") || name.matches("[0-9][0-9]w[0-9][0-9][a-z]") )) {
+					menu.add(new JMenuItem(name));
+				}
+				if (f.isFile() && (name.endsWith(".jar") || name.endsWith(".zip"))) {
+					global = true;
+				}
+			}
+			if (global) {
+				menu.insert(new JMenuItem("Global"), 0);
+			}
+		} else {
+			menu.setEnabled(false);
+		}
+		return menu;
+	}
+
+	private static JMenu createOpenVanillaMenu() {
+		JMenu menu = new JMenu("Open Vanilla");
+		menu.setIcon(Resources.loadPNGIconAsset("open-vanilla"));
+		File indexes = new File(minecraftDirectory, "assets/indexes");
+		if (indexes.exists()) {
+			for (File f : Paths.sort(indexes.listFiles())) {
+				String name = f.getName();
+				if (name.endsWith(".json")) {
+					menu.add(new JMenuItem(name.substring(0, name.length()-5)));
+				}
+			}
+		} else {
+			menu.setEnabled(false);
+		}
+		return menu;
 	}
 
 }
---
+++ src/main/java/com/gameminers/ethereal/browser/VersionComparator.java
@@ -0,0 +1,82 @@
+/*
+ *  EtheralBrowser
+ *  Copyright (C) 2014 Aesen Vismea
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.gameminers.ethereal.browser;
+
+import java.io.File;
+import java.text.Collator;
+import java.util.Comparator;
+
+public class VersionComparator implements Comparator<File> {
+	private static final String SNAPSHOT_REGEX = "[0-9][0-9]w[0-9][0-9][a-z]";
+	private static final String INITIAL_RELEASE_REGEX = "[0-9]*\\.[0-9]*";
+	private static final String PATCH_RELEASE_REGEX = "[0-9]*\\.[0-9]*\\.[0-9]*";
+
+	private Collator collator = Collator.getInstance();
+	
+	@Override
+	public int compare(File o1, File o2) {
+		String s1 = o1.getName();
+		String s2 = o2.getName();
+		if (s1.contains(".") && !o1.isDirectory()) {
+			s1 = s1.substring(0, s1.lastIndexOf('.'));
+		}
+		if (s2.contains(".") && !o2.isDirectory()) {
+			s2 = s2.substring(0, s2.lastIndexOf('.'));
+		}
+		boolean snapshot1 = s1.matches(SNAPSHOT_REGEX);
+		boolean snapshot2 = s2.matches(SNAPSHOT_REGEX);
+		boolean version1 = s1.matches(INITIAL_RELEASE_REGEX) || s1.matches(PATCH_RELEASE_REGEX);
+		boolean version2 = s2.matches(INITIAL_RELEASE_REGEX) || s1.matches(PATCH_RELEASE_REGEX);
+		if (snapshot1 != snapshot2) {
+			return snapshot1 ? 1 : -1;
+		} else if (snapshot1 && snapshot2) {
+			int snapshotId1 = getSnapshotId(s1);
+			int snapshotId2 = getSnapshotId(s2);
+			return Integer.compare(snapshotId1, snapshotId2);
+		} else if (s1.equals("legacy") != s2.equals("legacy")) {
+			return s1.equals("legacy") ? -1 : 1;
+		} else if (version1 && version2) {
+			int versionCode1 = getVersionCode(s1);
+			int versionCode2 = getVersionCode(s2);
+			return Integer.compare(versionCode1, versionCode2);
+		} else {
+			System.out.println("? vs ?");
+			return collator.compare(s1, s2);
+		}
+	}
+
+	private int getSnapshotId(String s) {
+		return Integer.valueOf(s.substring(0, 2)+s.substring(3, 5));
+	}
+
+	private int getVersionCode(String s) {
+		String[] split = s.split("\\.");
+		String major = split[0];
+		String minor = split[1];
+		String patch = (split.length >= 3 ? leftPad(split[2]) : "000");
+		return Integer.parseInt(major+minor+patch);
+	}
+	
+	private String leftPad(String string) {
+		if (string.length() < 3) {
+			string = "000".substring(0, 3-string.length())+string;
+		}
+		return string;
+	}
+}
+
